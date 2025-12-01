0ed720dfae0f0250627338707a259866c4a601dd
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-01 20:51:40 +0200

    algorithm for finding what operation we'll be doing next

--- calc.js
+++ calc.js
@@ -155,3 +155,22 @@ function normalization(code, count) {
     }
     return code;
 }
+
+function whatOperationWeDo(originalNumber, numberForFirstIndex) {
+    let operation;
+    let numberLookedFor;
+    if (originalNumber > 0 && numberForFirstIndex[0] == 0) {
+        operation = 1;
+        numberLookedFor = 1;
+    } else if (originalNumber > 0 && numberForFirstIndex[0] == 1) {
+        operation = 0;
+        numberLookedFor = 0;
+    } else if (originalNumber < 0 && numberForFirstIndex[0] == 0) {
+        operation = 0;
+        numberLookedFor = 0;
+    } else if (originalNumber < 0 && numberForFirstIndex[0] == 1) {
+        operation = 1;
+        numberLookedFor = 1;
+    }
+    return [operation, numberLookedFor];
+}
