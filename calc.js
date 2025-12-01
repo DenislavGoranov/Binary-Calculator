e61412a37c65dd56c7a09a4c46c8737ca691da0a
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-01 20:45:22 +0200

    form handling algorithm

--- calc.js
+++ calc.js
@@ -4,3 +4,15 @@ var isTrue;
 var firstNumberAdditionalCode = [];
 var secondNumberAdditionalCode = [];
 var thirdNumberAdditionalCode = [];
+// function getNumbers(event) {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+//     const numbersData = Object.fromEntries(formData);
+
+//     event.target.reset();
+//     num1 = Number(numbersData.first);
+//     num2 = Number(numbersData.second);
+//     bit = Number(numbersData.third);
+
+//     calculations(num1, num2);
+// }
