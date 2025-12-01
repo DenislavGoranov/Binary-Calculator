981ca05d89356b7792d8842466a302aea2c9d764
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-01 20:48:42 +0200

    find additional code algorithm

--- calc.js
+++ calc.js
@@ -75,3 +75,20 @@ function findReversedCode(straightCode) {
 
     return reversedCode;
 }
+
+function findAdditionalCode(reversedCode) {
+    let prenos = 1;
+    for (let i = bit - 1; i >= 0; i--) {
+        if (reversedCode[i] == 0 && prenos == 1) {
+            reversedCode[i] = 1;
+            break;
+        } else if (reversedCode[i] == 1 && prenos == 1) {
+            reversedCode[i] = 0;
+            prenos = 1;
+        } else if (reversedCode[i] == 1 && prenos == 0) {
+            reversedCode[i] = 1;
+            break;
+        }
+    }
+    return reversedCode;
+}
