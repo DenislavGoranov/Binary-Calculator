731ed6c2067ab4adda827d5765a6acbdbf3e8cf0
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-01 20:50:44 +0200

    normalization/moving algorithm

--- calc.js
+++ calc.js
@@ -138,3 +138,20 @@ function binarySummary(firstNumberAdditionalCode, secondNumberAdditionalCode) {
 
     return result;
 }
+
+function normalization(code, count) {
+    if (count == 0) {
+        return code;
+    }
+    let count2 = count;
+    while (count2 != 0) {
+        for (let i = 0; i < code.length; i++) {
+            code[i] = code[i + 1];
+        }
+        count2--;
+    }
+    for (let i = code.length - count; i < code.length; i++) {
+        code[i] = 0;
+    }
+    return code;
+}
