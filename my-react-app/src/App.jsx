4889ac4130a59f7579a3f6c2833a88862998324c
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-05 23:11:54 +0200

    add the new components in the app.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,38 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+
+import HomePage from './components/HomePage'
+
 import './home.css'
+
 function App() {
 
     return (
-        <div className="page">
-            <nav>
-                <ul>
-                    <li><a href="/binary_sum.html">Calculator</a></li>
-                    <li><a href="">Explanations</a></li>
-                    <li><a href="">Sources</a></li>
-                </ul>
-            </nav>
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/explanations' />
+            <Route path='/about' />
+        </Routes>
 
-            <h1>Binary Calculator</h1>
-
-            <form method="get">
-                <div>
-                    <label>Value 1</label>
-                    <input type="text" name="first" />
-                </div>
-
-                <div className="operations">
-                    <button><i className="fa-solid fa-plus"></i></button>
-                    <button><i className="fa-solid fa-xmark"></i></button>
-                    <button><i className="fa-solid fa-divide"></i></button>
-                </div>
-
-                <div>
-                    <label>Value 2</label>
-                    <input type="text" name="second" />
-                </div>
-
-                <input type="submit" value="Calculate" />
-            </form>
-        </div>
     )
 }
 
