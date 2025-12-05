f164495d9edc7127d8627c61a9a654b352c9ee20
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-05 22:56:03 +0200

    fix css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import './home.css'
-
 function App() {
 
     return (
-        <>
+        <div className="page">
             <nav>
                 <ul>
                     <li><a href="/binary_sum.html">Calculator</a></li>
@@ -17,25 +12,27 @@ function App() {
             </nav>
 
             <h1>Binary Calculator</h1>
-            <form method="get" onsubmit="getNumbers(event)">
+
+            <form method="get">
                 <div>
-                    <label for="">Value 1</label>
+                    <label>Value 1</label>
                     <input type="text" name="first" />
                 </div>
-                <div class="operations">
-                    <button><i class="fa-solid fa-plus"></i></button>
-                    <button><i class="fa-solid fa-xmark"></i></button>
-                    <button><i class="fa-solid fa-divide"></i></button>
+
+                <div className="operations">
+                    <button><i className="fa-solid fa-plus"></i></button>
+                    <button><i className="fa-solid fa-xmark"></i></button>
+                    <button><i className="fa-solid fa-divide"></i></button>
                 </div>
+
                 <div>
-                    <label for="">Value 2</label>
+                    <label>Value 2</label>
                     <input type="text" name="second" />
                 </div>
 
-
                 <input type="submit" value="Calculate" />
             </form>
-        </>
+        </div>
     )
 }
 
