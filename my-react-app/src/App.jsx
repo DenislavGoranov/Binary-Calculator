a5f4f6bfb61b3f835ca1722783da67557188550d
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-05 22:30:37 +0200

    import home page and the css for home page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,34 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './home.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><a href="/binary_sum.html">Calculator</a></li>
+                    <li><a href="">Explanations</a></li>
+                    <li><a href="">Sources</a></li>
+                </ul>
+            </nav>
+
+            <h1>Binary Calculator</h1>
+            <form method="get" onsubmit="getNumbers(event)">
+                <div>
+                    <label for="">Value 1</label>
+                    <input type="text" name="first" />
+                </div>
+                <div class="operations">
+                    <button><i class="fa-solid fa-plus"></i></button>
+                    <button><i class="fa-solid fa-xmark"></i></button>
+                    <button><i class="fa-solid fa-divide"></i></button>
+                </div>
+                <div>
+                    <label for="">Value 2</label>
+                    <input type="text" name="second" />
+                </div>
+
+
+                <input type="submit" value="Calculate" />
+            </form>
+        </>
+    )
 }
 
 export default App
