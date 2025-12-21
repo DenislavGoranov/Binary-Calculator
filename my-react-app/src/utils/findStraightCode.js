e9c4334c879f22b79ce5089e4d4c3db44723a05f
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-21 14:21:27 +0200

    create util for finding straight code

---
+++ my-react-app/src/utils/findStraightCode.js
@@ -0,0 +1,27 @@
+export default function findStraightCode(positiveNumber, originalNumber, bit) {
+    let countForNormalization = 0;
+    let straightCode = [];
+    while (positiveNumber != 0) {
+        if (positiveNumber % 2 == 0) {
+            straightCode.push(0);
+        } else {
+            straightCode.push(1);
+        }
+    
+        positiveNumber = Math.floor(positiveNumber / 2);
+    }
+    
+    straightCode.reverse();
+    
+    while (straightCode.length < bit - 1) {
+        straightCode.unshift(0);
+        countForNormalization++;
+    }
+
+    if (originalNumber > 0) {
+        straightCode.unshift(0);
+    } else {
+        straightCode.unshift(1);
+    }
+    return [straightCode, countForNormalization];
+}
