0f0b5a727570cb957fa9e3cc27a4da5b2421643d
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-12-21 15:00:27 +0200

    add functionality in plus component

--- my-react-app/src/components/ResultPage.jsx
+++ my-react-app/src/components/ResultPage.jsx
@@ -1,35 +1,14 @@
 import Header from "./Header";
-
-import useCalcData from "../hooks/useCalcData";
-import binarySummary from "../utils/binarySummary";
 import PlusPage from "./operationComponents/PlusPage";
-export default function ResultPage() {
-    const [calcData] = useCalcData();
-    if (calcData.operation == 'plus') {
-        console.log(calcData.value1);
-        console.log(calcData.value2);
-        console.log(binarySummary(calcData.value1, calcData.value2, calcData.bits));
-    }
+import MultiplyPage from "./operationComponents/MultiplyPage";
+import DividePage from "./operationComponents/DividePage";
 
+export default function ResultPage() {
     return (
         <div className="page">
             <Header />
 
-            {calcData.operation === 'plus' ? (
-                <PlusPage />
-            ) : (
-                <div>mistake</div>
-            )}
-            {calcData.operation === 'multiply' ? (
-                <MultiplyPage />
-            ) : (
-                <div>mistake</div>
-            )}
-            {calcData.operation === 'divide' ? (
-                <DividePage />
-            ) : (
-                <div>mistake</div>
-            )}
+            <PlusPage />
         </div>
     );
 }
--- my-react-app/src/components/operationComponents/PlusPage.jsx
+++ my-react-app/src/components/operationComponents/PlusPage.jsx
@@ -1,5 +1,17 @@
+import useCalcData from "../../hooks/useCalcData";
+import useSummaryData from "../../hooks/useSummaryData";
+
 export default function PlusPage() {
+    const [calcData] = useCalcData();
+    const result = useSummaryData(calcData.value1, calcData.value2, calcData.bits)
+
     return (
-        <div className="page">pluscheee</div>
+        <div className="page">
+            <h1>{calcData.value1}</h1>
+            <h1>+</h1>
+            <h1>{calcData.value2}</h1>
+            <h1>=</h1>
+            <h1>{result}</h1>
+        </div>
     );
 }
